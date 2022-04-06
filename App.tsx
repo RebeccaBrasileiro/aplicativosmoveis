2784ce46124c7de74a8ff3d90ec92ada5784f6be
RebeccaBrasileiro <rebeccabrasileiro28@gmail.com>
2022-04-06 08:44:25 -0300

    app

--- App.tsx
+++ App.tsx
@@ -1,20 +1,5 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import Login from "./src/screens/Login";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Aplicações Moveis!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+export default function App(){
+  return <Login />
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#882233',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
